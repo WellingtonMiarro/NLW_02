aa24f88f2c2cff286a0a3769b1d59790fb1b6853
Wellington Miarro <whellingtonmiarroferreira@gmail.com>
2020-08-12 19:22:40 -0400

    Public

---
+++ public/scripts/addField.js
@@ -0,0 +1,14 @@
+//procurar o botão                    //Quando Clicar no botão
+document.querySelector("#add-time").addEventListener("click", cloneField);
+
+//Executar uma ação
+function cloneField() {
+  const newFieldContainer = document
+    .querySelector(".schedule-item")
+    .cloneNode(true);
+  const fields = newFieldContainer.querySelectorAll("input");
+  fields.forEach(function (field) {
+    field.value = "";
+  });
+  document.querySelector("#schedule-items").appendChild(newFieldContainer);
+}
